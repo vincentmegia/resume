cbd25007a852ae1ce274891b8a41daa9aa441b91
vincentmegia <vincent.megia@gmail.com>
2016-07-02 22:29:06 +0800

    changed startup from boot.ts --> boot.js

--- IamStupendous.Resume/src/IamStupendous.Resume/wwwroot/scripts/systemjs.config.js
+++ IamStupendous.Resume/src/IamStupendous.Resume/wwwroot/scripts/systemjs.config.js
@@ -15,7 +15,7 @@
 
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
-        'app': { main: 'boot.ts', defaultExtension: 'ts' },
+        'app': { main: 'boot.js', defaultExtension: 'js' },
         'rxjs': { defaultExtension: 'js' },
         'angular2-in-memory-web-api': { defaultExtension: 'js' },
         'bootstrap': { defaultExtension: 'js' },
